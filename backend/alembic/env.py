7dbf91e5aaad375bc3b93912ac7f8f6325047617
Iris_duyendt <duyendt@codenet.jp>
2023-06-28 12:00:12 +0700

    update be install

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -22,8 +22,7 @@ config.set_section_option(section, "POSTGRES_PORT", _env("POSTGRES_PORT"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -37,7 +36,7 @@ target_metadata = None
 # ... etc.
 
 
-def run_migrations_offline() -> None:
+def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -61,7 +60,7 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
+def run_migrations_online():
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
