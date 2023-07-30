93d90de203ef5b372a7b0dfbd3c6e1cb6b555805
duyendtcidm <duyendtcidm@gmail.com>
2023-07-30 19:41:28 +0700

    init test page

---
+++ frontend/src/components/Quiz/index.vue
@@ -0,0 +1,85 @@
+<template lang="pug">
+  v-card.mx-auto.mt-5(max-width='800' elevation='4')
+    v-card-title.justify-center.rounded-card.mb-1(style='background-color: #81D4FA' primary-title='')
+      h1.font-weight-thin Multiple Choice Quiz
+    v-card-text.pt-2(primary-title='')
+      p Currently answering question Nr:{{this.currentIndex +1}}
+    //v-img.white--text.align-end(height='200px' src='../assets/multiple_choice.jpg')
+    v-spacer
+    v-card(style='background-color: #E1F5FE')
+      v-card-text()
+        //v-if='loading'
+        h4.justify-center.font-italic Data is being loaded one moment please...
+      v-card-text()
+        //v-else=''
+        h4.justify-center.font-italic {{this.currentQuestion}}
+      v-spacer
+      v-card-actions(rules='required')
+        v-radio-group(v-model='answered')
+          v-radio(v-for='n in this.shuffledAnswers' :key='n' :label='`${n}`' :value='n')
+    v-card-actions.d-flex.justify-space-around
+      v-btn(@click='skip' elevation='2' color='error' size='x-large')
+        | Exit
+      v-btn(@click='next' elevation='2' color='green' size='x-large')
+        | Next
+</template>
+
+<script>
+import {defineComponent, getCurrentInstance, ref, toRefs} from 'vue'
+import { PRACTICE_HEADER } from '../index'
+
+export default defineComponent ({
+  components: {
+
+  },
+  props: {
+    quizes: {
+      type: Object,
+      required: false
+    }
+  },
+  setup(props) {
+    const questions = toRefs(props.quizes)
+    const currentIndex = ref(1)
+    const currentQuestion = ref(false)
+    const answered = ref(false)
+    const shuffledAnswers = ref(false)
+
+    const skip = () => {
+
+    }
+    const next = () => {
+
+    }
+
+    return {
+      questions,
+      currentIndex,
+      currentQuestion,
+      answered,
+      shuffledAnswers,
+      skip,
+      next
+    }
+  }
+})
+
+</script>
+
+<style scoped lang="sass">
+@import '@/style/css/common'
+.question-part
+  margin: auto
+  width: 700px
+.choice
+  width: 100%
+  button
+    width: 40%
+.right-answer
+  background-color: green
+  color: green
+.wrong-answer
+  background-color: red
+
+
+</style>
--- frontend/src/pages/Test/index.vue
+++ frontend/src/pages/Test/index.vue
@@ -1,29 +1,119 @@
 <template lang="pug">
-  v-btn(@click="onRender") hellooo
+  div
+    v-card(v-if="!isRendered" ).mx-auto.mt-4(max-width='700' style='background-color: #fff' height='95%')
+      v-card-title.justify-center.rounded-card.mb-1(style='background-color: #81D4FA')
+        h1.font-weight-thin {{$t('test.test_type.title')}}
+      v-img.white--text.align-end(height='150px' src='@/assets/quiz.jpg')
+      v-spacer
+      v-card-subtitle
+        h2.display-1 {{$t('test.test_type.subtitle')}}
+      v-card-actions.standard
+        v-text-field.text-field(
+          v-model='amount'
+          type='number'
+          hide-spin-buttons
+          :label="$t('test.test_type.amount')"
+          dense
+        )
+      //v-card-actions
+      //  v-radio-group(
+      //
+      //    v-model='level'
+      //  )
+      //    v-radio(label='N1' value='N1')
+      //    v-radio(label='N2' value='N2')
+      //    v-radio(label='N3' value='N3')
+      //    v-radio(label='N4' value='N4')
+      //    v-radio(label='N5' value='N5')
+      v-select.standard.px-2(
+        v-model='level'
+        :items='levelTypeItems'
+        :label="$t('test.test_type.level')"
+        dense
+        outlined
+      )
+      v-select.standard.px-2(
+        v-model='question_type'
+        dense
+        :items='questionTypeItems'
+        :label="$t('test.test_type.type_question')"
+        outlined
+      )
+      v-row.standard
+        v-btn.ma-2.white--text(elevation='2' color='info' :disabled="!level || amount === '0' || !amount || !question_type" @click='onRender')
+          router-link(style='text-decoration: none; color: inherit;' to=`/test/?level=${level}&amount=${amount}&question_type=${question_type}`)
+          | {{$t('test.start_test')}}
+    div(v-if="isRendered")
+      quiz(quizes="questions")
+
+
 
 </template>
 
 <script>
 import { defineComponent, getCurrentInstance, ref } from 'vue'
 import {api, i18n} from '@/plugins'
+import { Quiz } from '@/components'
 
 export default defineComponent({
+  components: {
+    Quiz,
+  },
 
   setup() {
-    const level = 'N3'
-    const amount = 15
-    const question_type = 'hiragana'
-    const onRender = async (searchInfo) => {
+    const instance = getCurrentInstance()
+    const { $toast, $root, $refs } = instance.proxy
+    const level = ref('')
+    const amount = ref(0)
+    const question_type = ref('')
+    const levelTypeItems = ['N1', 'N2', 'N3', 'N4', 'N5']
+    const isRendered = ref(false)
+    const questions = ref([])
+
+    const questionTypeItems = [
+        'Lựa chọn cách đọc đúng của từ trong ngoặc',
+        'Viết lại từ trong ngoặc bằng chữ hán',
+        'Lựa chọn từ thích hợp điển vào chỗ trống',
+        'Tìm từ đồng nghĩa với từ trong ngoặc',
+        'Lựa chọn câu có cách sử dụng đúng của từ'
+    ]
+
+    const defineQuestionType = (question_type) => {
+      if(question_type === 'Lựa chọn cách đọc đúng của từ trong ngoặc')
+        return 'hiragana'
+      else if (question_type === 'Viết lại từ trong ngoặc bằng chữ hán')
+          return 'kanji'
+      else if (question_type === 'Lựa chọn từ thích hợp điển vào chỗ trống')
+          return 'missing_word'
+      else if (question_type === 'Tìm từ đồng nghĩa với từ trong ngoặc')
+          return 'synonym'
+      else if (question_type === 'Lựa chọn câu có cách sử dụng đúng của từ')
+          return 'true_meaning'
+    }
+
+    const onRender = async () => {
+      isRendered.value = false
       try {
-        const { data } = await api.get(`/test/get_question_to_practice?level=${level}&amount=${amount}&question_type=${question_type}` )
+        const questionType = defineQuestionType(question_type.value)
+        const { data } = await api.get(`/test/get_question_to_practice?level=${level.value}&amount=${amount.value}&question_type=${questionType}` )
         console.log(data)
+        questions.value = data
+        console.log(questions.value, typeof questions.value)
+        isRendered.value = true
       } catch (e) {
         console.log('hihi', e)
       }
     }
 
     return {
-      onRender
+      level,
+      amount,
+      question_type,
+      levelTypeItems,
+      questionTypeItems,
+      onRender,
+      isRendered,
+      questions
     }
   }
 
@@ -32,5 +122,10 @@ export default defineComponent({
 </script>
 
 <style lang="sass" scoped>
-
+.rounded-card
+  border-radius: 5px
+.standard
+  width: 50%
+  margin: auto
+  justify-content: center
 </style>
